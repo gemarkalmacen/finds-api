a2e5d73609c13cd12af2a1a5de523e0ad0d41b58
Gemark Almacen <gmrk08@gmail.com>
2021-12-27 17:36:29 +0800

    initial update

--- app/Http/Controllers/Api/V1/Staff/AuthController.php
+++ app/Http/Controllers/Api/V1/Staff/AuthController.php
@@ -63,6 +63,10 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request){
+        auth()->user()->tokens()->delete();
 
+        return [
+            
+        ];
     }
 }
